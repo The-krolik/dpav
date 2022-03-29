ec1ebbb0f2ee0a9ddb1e7ad356e2db3803100b73
cda5b <91585402+cda5b@users.noreply.github.com>
2022-03-29 10:44:46 -0500

    Updated visual.py
    added a few comments that will be updated in the future for documentation's sake

--- visual.py
+++ visual.py
@@ -1,21 +1,27 @@
-#Visual Buffer Team: Christopher Andrews
-#                    Ian Hurd
-#                    Angelo Tammaro
+# Visual Buffer Team: Christopher Andrews
+#                     Ian Hurd
+#                     Angelo Tammaro
 
 import numpy as np
 
 class Visual:
     
     def __init__(self):
-        #TODO
+        # TODO
+        # imageArray: a numpy array that holds the pixel data of a picture
         imageArray = np.array()
+        
+        # debugFlag: a boolean variable
         debugFlag = False
         
+    # <insert definition of writePixel function here>; void function
     def writePixel():
-        #TODO
+        # TODO
         
+    # <insert definition of getDimensions function here>; X and Y are int variables
     def getDimensions(X, Y):
-        #TODO
+        # TODO
         
+    # <insert definition of clearBuffer function here>; void function
     def clearBuffer():
-        #TODO
+        # TODO
