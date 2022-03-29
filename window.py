24b76a168d793233fe24585bef9f2a1384ede57b
chandlerstewart <user email>
2022-03-29 10:06:52 -0500

    Window Class Skeleton

---
+++ window.py
@@ -0,0 +1,21 @@
+
+class Window:
+    
+    def __init__(self):
+        
+        VisualBuffer = None       #temp
+        EventDictionary = None    #temp
+        DebugFlag = None          #temp
+        
+    
+    def GetMousePosition(self):
+        return None               #temp
+    
+    def SetVisualBuffer(self, VisualBuffer):
+        return None               #temp
+    
+    def Open(self):
+        return None               #temp
+    
+    def Close(self):
+        return None               #temp
