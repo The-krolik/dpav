b4c08974bba10d0be8bd59b54e5cb863be128ae1
cda5b <91585402+cda5b@users.noreply.github.com>
2022-03-29 10:28:12 -0500

    Created visual.py
    Impromptu skeleton file for the Visual Buffer team

---
+++ visual.py
@@ -0,0 +1,21 @@
+#Visual Buffer Team: Christopher Andrews
+#                    <insert your name here>
+#                    <insert your name here>
+
+import numpy as np
+
+class Visual:
+    
+    def __init__(self):
+        #TODO
+        imageArray = np.array()
+        debugFlag = False
+        
+    def writePixel():
+        #TODO
+        
+    def getDimensions(X, Y):
+        #TODO
+        
+    def clearBuffer():
+        #TODO
