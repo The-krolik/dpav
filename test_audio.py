e1fc32b1b91daa81bb3c0120e5fcc798a20e722e
Cody <chavilan@icloud.com>
2022-03-31 16:15:14 -0500

    more audio tests

--- test_audio.py
+++ test_audio.py
@@ -1,10 +1,40 @@
 import audio as aud
 
-def test_getBitNumber():
-  a = aud.Audio()
-  assert a.getBitNumber() == 16
-  
-def test_setBitNumber():
-  a = aud.Audio()
-  a.setBitNumber(8)
-  assert a.getBitNumber() == 8
+def TestAudio():
+    a = aud.Audio()
+
+
+    # bit number tests
+    assert a.getBitNumber() == 16
+
+    a.setBitNumber(8)
+    assert a.getBitNumber() == 8
+
+    a.setBitNumber(32)
+    assert a.getBitNumber() == 32
+
+    a.setBitNumber(33)
+    assert a.getBitNumber() == 8
+
+
+    # sample rate tests
+    assert a.getSampleRate() == 44100
+
+    # Are other sample rates supported?
+
+
+    # audio buffer tests
+
+
+    # audio device tests
+    a.setAudioDevice(1)
+    assert a.getAudioDevice == 1
+
+    a.setAudioDevice(-1)
+    assert a.getAudioDevice == 1
+
+    a.setAudioDevice(9999)
+    assert a.getAudioDevice == 1
+
+
+    # playSound tests
