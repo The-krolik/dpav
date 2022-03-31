d3b9a84a10fe6edd1049010edbe11df7eed7c2d3
Eric Ramsay <eric.the.ramsay@gmail.com>
2022-03-31 10:40:02 -0500

    Updated class definitions

--- eric_window.py
+++ eric_window.py
@@ -4,21 +4,29 @@ import pygame
 class Window:
     
     def __init__(self):
-        
-        VisualBuffer = None
-        EventDictionary = None
-        DebugFlag = None
-		Screen = None
+        self.VB = np.zeros((600,600), 3)			# Temporary until VB class finished
+        self.EventDictionary = None
+        self.DebugFlag = None
+		self.Screen = None
+		self.Open = False
            
     def GetMousePosition(self):
         return pygame.mouse.get_pos()
     
     def SetVisualBuffer(self, VisualBuffer):
-        VisualBuffer = VisualBuffer
+        self.VB = VisualBuffer
+		if self.Open:
+			self.Screen.blit(VB, (0, 0))
+			pygame.display.flip()
     
-    def Open(width, height):
-        Screen = pygame.display.set_mode((width, height))
-		VisualBuffer = VisualBuffer(width, height)
+    def Open(self):
+		self.Open = True
+		
+        self.Screen = pygame.display.set_mode(self.VB.getDimensions())
+		pygame.init()
+		
+		pygame.display.flip()
     
     def Close(self):
+		self.Open = False
         pygame.display.quit()
