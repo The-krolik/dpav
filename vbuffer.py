7f790a4b6e5aa69533bcf9b7a27dacfe968394cc
Angelo C. Tammaro <chaztammaro@yahoo.com>
2022-04-05 11:58:21 -0500

    Update vbuffer.py

--- vbuffer.py
+++ vbuffer.py
@@ -2,10 +2,7 @@
 #                     Ian Hurd
 #                     Angelo Tammaro
 
-<<<<<<< HEAD
 from typing import Type
-=======
->>>>>>> 266851bdff7c6f4dc1c276314282daf9434dfa6f
 import numpy as np
 
 class VBuffer:
@@ -13,19 +10,11 @@ class VBuffer:
     def __init__(self, dimensions):
         # TODO
         # imageArray: a numpy array that holds the pixel data of a picture
-        if type(dimensions) is not list and type(dimensions) is not tuple:
-            raise TypeError("Error! Dimensions is supposed to be a list or tuple!")
-        elif len(dimensions) != 2:
-            raise ValueError("Error! Dimensions are only meant to be 2 values!")
-<<<<<<< HEAD
-        elif type(dimensions[0]) is not int and type(dimensions[1]) is not int:
-=======
-        elif type(dimensions[0]) is not int or type(dimensions[1]) is not int:
->>>>>>> 266851bdff7c6f4dc1c276314282daf9434dfa6f
-            raise TypeError("Error! Dimensions are meant to be integer values! ")
 
-
+        self._checkDoubleIntVal(dimensions, "dimensions", "__init__")
         xdim, ydim = dimensions[0], dimensions[1]
+        if xdim <= 0 or ydim <= 0:
+            
         listGrid = [[0 for i in range(xdim)] for j in range(ydim)]
         self._buffer = np.array(listGrid)
         self._dimensions = (xdim, ydim)
@@ -35,6 +24,8 @@ class VBuffer:
 
         
     # Sets pixel at coordinates coords in buffer to hex value val
+    #error check correct color value greater than 0 less than 2^32 
+    #error check if unsigned int
     def writePixel(self, coords, val):
         x, y = coords[0], coords[1]
         self._buffer[x, y] = val
@@ -45,7 +36,6 @@ class VBuffer:
         
     # <insert definition of getDimensions function here>; X and Y are int variables
     def getDimensions(self):
-<<<<<<< HEAD
         try:
             return self._dimensions
         except TypeError:
@@ -53,11 +43,6 @@ class VBuffer:
         except ValueError:
             raise ValueError("Error! The values of x and y are meant to be integer values!")
 
-=======
-        return self._dimensions
-        
-        
->>>>>>> 266851bdff7c6f4dc1c276314282daf9434dfa6f
     # 
     def clearBuffer(self):
         self._buffer[:] = 0
