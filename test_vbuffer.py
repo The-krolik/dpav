a42a8cfb8b2c948b411e481b3f15779ed70b73ff
The-krolik <100061578+The-krolik@users.noreply.github.com>
2022-04-05 10:18:58 -0500

    Update test_vbuffer.py

--- test_vbuffer.py
+++ test_vbuffer.py
@@ -1,7 +1,47 @@
-import vbuffer as vbuf
+import directpythonplatform as dpp
+
+def test_audio():
+    a = audio.Audio()
+
+
+    # bit number tests
+    assert a.getBitNumber() == 16
+
+    a.setBitNumber(8)
+    assert a.getBitNumber() == 8
+
+    a.setBitNumber(32)
+    assert a.getBitNumber() == 32
+
+    a.setBitNumber(33)
+    assert a.getBitNumber() == 8
+
+
+    # sample rate tests
+    assert a.getSampleRate() == 44100
+
+    # Are other sample rates supported?
+
+
+    # audio buffer tests
+
+
+    # audio device tests
+    a.setAudioDevice(1)
+    assert a.getAudioDevice == 1
+
+    a.setAudioDevice(-1)
+    assert a.getAudioDevice == 1
+
+    a.setAudioDevice(9999)
+    assert a.getAudioDevice == 1
+
+
+    # playSound tests
+    
 
 def test_vbuffer():
-    vb = vbuf.VBuffer()
+    vb = vbufer.VBuffer()
     x, y = vb.getDimensions()
     assert x > 0
     assert y > 0
