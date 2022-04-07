6288f8843dd303f814d6bfa468c3cde1606679b2
The-krolik <100061578+The-krolik@users.noreply.github.com>
2022-04-07 16:12:28 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -27,11 +27,11 @@ def test_audio():
     a.setAudioDevice(1)
     assert a.getAudioDevice() == 1
 
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         a.setAudioDevice(-1)
     assert a.getAudioDevice() == 1
     
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         a.setAudioDevice(1.5)
     assert a.getAudioDevice() == 1
     
@@ -56,19 +56,19 @@ def test_vbuffer():
     assert type(x) is int
     assert type(y) is int
 
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([0, 0])
         
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([9999, 9999])
     
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([-100, -100])
         
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([100.9, 200])
         
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([200, 200.5])
         
     vb = dpp.VBuffer([800, 600])
@@ -81,23 +81,23 @@ def test_vbuffer():
     vb = dpp.VBuffer([800, 600])
     assert vb.getPixel([400, 300]) == 0
     
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb.writePixel([-300, 9], 16777215)
     
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb.writePixel([300, -200], 16777215)
         
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb.writePixel([300.1, 200], 16777215)
         
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb.writePixel([20, 799.5], 16777215)
     
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb.writePixel([399, 299], 16777216)
     assert vb.getPixel([399, 299]) == 0
 
-    with pytest.raises(Exception) as e_info:
+    with pytest.raises(ValueError) as e_info:
         vb.writePixel([399, 299], 100.5)
     assert vb.getPixel([399, 299]) == 0
 
