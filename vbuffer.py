95ca62287a8c032b69ab08e8e4612a5fd1f67cae
cda5b <91585402+cda5b@users.noreply.github.com>
2022-04-07 10:08:45 -0500

    Update vbuffer.py
    added some quick comments for the functions

--- vbuffer.py
+++ vbuffer.py
@@ -6,9 +6,9 @@ import numpy as np
 
 class VBuffer:
     
+    # Constructor; Called each time a VBuffer object is created; Initializes member data;
     def __init__(self, dimensions):
-        # TODO
-        # imageArray: a numpy array that holds the pixel data of a picture
+        # error checking
         if type(dimensions) is not list and type(dimensions) is not tuple:
             raise TypeError("Error! Dimensions is supposed to be a list or tuple!")
         elif len(dimensions) != 2:
@@ -16,13 +16,11 @@ class VBuffer:
         elif type(dimensions[0]) is not int or type(dimensions[1]) is not int:
             raise TypeError("Error! Dimensions are meant to be integer values! ")
 
-
         xdim, ydim = dimensions[0], dimensions[1]
         listGrid = [[0 for i in range(xdim)] for j in range(ydim)]
         self._buffer = np.array(listGrid)
         self._dimensions = (xdim, ydim)
         
-        # debugFlag: a boolean variable
         self.debugFlag = False
 
         
@@ -31,20 +29,20 @@ class VBuffer:
         x, y = coords[0], coords[1]
         self._buffer[x, y] = val
 
+    # Function used to get a specific pixel through the use of specified coordinates passed to the function as an array
     def getPixel(self, coords):
         x, y = coords[0], coords[1]
         return self._buffer[x, y]
         
-    # <insert definition of getDimensions function here>; X and Y are int variables
+    # Function used to get the dimensions of the current image data
     def getDimensions(self):
         return self._dimensions
         
         
-    # 
+    # Function used to clear the visual buffer; Sets all elements to zero
     def clearBuffer(self):
         self._buffer[:] = 0
 
+    # Function used to get the current visual buffer
     def getBuffer(self):
         return self._buffer
-        
-    
