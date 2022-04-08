0e8f3270528f4edbd56c5fb096c379085f06fff6
The-krolik <100061578+The-krolik@users.noreply.github.com>
2022-04-07 20:35:28 -0500

    Update vbuffer.py
    - renamed error checking functions for readability
    - restricted to 8-bit color due to limitations with the window class

--- vbuffer.py
+++ vbuffer.py
@@ -11,7 +11,7 @@ class VBuffer:
         # TODO
         # imageArray: a numpy array that holds the pixel data of a picture
 
-        self._checkDoubleIntVal(dimensions, "dimensions", "__init__")
+        self._checkCoordType(dimensions, "dimensions", "__init__")
         xdim, ydim = dimensions[0], dimensions[1]
         if xdim <= 0 or ydim <= 0:
             raise ValueError("Buffer dimensions in vBuffer must exceed 0!")
@@ -25,37 +25,36 @@ class VBuffer:
         self.debugFlag = False
 
 
-    def _checkDoubleIntVal(self, val, argName, methodName):
+    def _checkCoordType(self, coords, argName, methodName):
         if type(val) is not list and type(val) is not tuple:
             raise TypeError(f"{argName} argument to vBuffer. {methodName} should be a list or a tuple!")
         elif len(val) != 2:
             raise TypeError(f"{argName} argument to vBuffer. {methodName} requires exactly 2 values!")
         elif type(val[0]) is not int or type(val[1]) is not int:
             raise TypeError(f"{argName} argument to vBuffer. {methodName} can only have integer values!")
-
-    def _checkCoordVals(self, x, y, methodName):
+        
+    def _checkCoordVal(self, coords, methodName):
         if x < 0 or y < 0:
             raise ValueError(f"Coordinate args to VBuffer.{methodName} should be greater than zero.")
         elif x >= self._dimensions[0] or y >= self.dimensions[1]:
             raise ValueError(f"Coordinate args to VBuffer.{methodName} are out of bounds.")
         
     # Sets pixel at coordinates coords in buffer to hex value val
-    #error check correct color value greater than 0 less than 2^24 
+    #error check correct color value greater than 0 less than 2^8 
     #error check if int
     def writePixel(self, coords, val):
-        self._checkDoubleIntVal(coords, "coords", "writePixel")
+        self._checkCoordType(coords, "coords", "writePixel")
+        self._checkCoordVal(coords, "coords", "writePixel")
         if type(val) is not int:
             raise TypeError("Color value must be an integer value!")
-        elif val < 0 or val > 2**24:
-            raise ValueError("Color value must remain an integer between 0 and 2^24!")
+        elif val < 0 or val > 255:
+            raise ValueError("Color value must be an integer between 0 and 255!")
+            
         x, y = coords[0], coords[1]
-        self._checkCoordVals(x, y, 'writePixel')
         self._buffer[x, y] = val
 
     def getPixel(self, coords):
-        self._checkDoubleIntVal(coords, "coords", "getPixel")
         x, y = coords[0], coords[1]
-        self._checkCoordVals(x, y, 'getPixel')
         return self._buffer[x, y]
         
     # <insert definition of getDimensions function here>; X and Y are int variables
