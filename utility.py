360830b64b49bd114e163d5d54a3ce1a7458eff1
chandlerstewart <user email>
2022-04-09 01:10:09 -0500

    added utility functions for getting an image from disk and converting from rgb to hex arrays

--- utility.py
+++ utility.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import numpy as np
+from PIL import Image
 
 def _debugOut(msg):
 
@@ -10,3 +12,30 @@ def _debugOut(msg):
     filename = "./logs/{}.txt".format(date)
     with open(filename, 'a') as file:
         file.write(msg)
+
+        
+'''
+Description:
+    Takes the file path of an image and returns an np.ndarray in hex
+'''
+def loadImage(filepath) -> np.ndarray:
+    image = Image.open(filepath)
+    image_array = np.array(image)
+    return rgb2hex(image_array).T
+
+'''
+Description:
+    Takes an np.ndarray in rgb and returns it in hex
+'''
+def rgb2hex(arr):
+    ret = np.zeros((arr.shape[0],arr.shape[1]))
+    
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            red = arr[i,j,0] << 16
+            green = arr[i,j,1] << 8
+            blue = arr[i,j,2]
+            ret[i,j] = red + green + blue
+                       
+                       
+    return ret
