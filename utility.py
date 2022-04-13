b90a910001165906e8f8ff4d23a136934b72a671
Cody <chavilan@icloud.com>
2022-04-12 23:28:28 -0500

    added draw_circle function)

--- utility.py
+++ utility.py
@@ -4,8 +4,7 @@ import numpy as np
 import pygame
 
 
-def _debugOut(msg):
-
+def _debug_out(msg):
     date_time = datetime.now()
     date = date_time.strftime("%Y%m%d")
     time = date_time.strftime("%H:%M:%S")
@@ -15,7 +14,7 @@ def _debugOut(msg):
     with open(filename, 'a') as file:
         file.write(msg)
 
-def drawRect(vbuffer, color, pt1, pt2):
+def draw_rect(vbuffer, color, pt1, pt2):
     
     pts = [pt1,pt2]
     
@@ -45,20 +44,21 @@ def drawRect(vbuffer, color, pt1, pt2):
     
     buf[lowx:highx, lowy:highy] = color
         
-'''
-Description:
-    Takes the file path of an image and returns an np.ndarray in hex
-'''
-def loadImage(filepath) -> np.ndarray:
+def load_image(filepath) -> np.ndarray:
+    """
+    Description:
+        Takes the file path of an image and returns an np.ndarray in hex
+    """
     imagesurf = pygame.image.load(filepath)
     image_array = pygame.surfarray.array3d(imagesurf)
     return rgb2hex(image_array)
 
-'''
-Description:
-    Takes an np.ndarray in rgb and returns it in hex
-'''
-def rgb2hex(arr):
+
+def rgb_to_hex(arr):
+    """
+    Description:
+        Takes an np.ndarray in rgb and returns it in hex
+    """
     ret = np.zeros((arr.shape[0],arr.shape[1]))
     
     for i in range(len(arr)):
@@ -68,5 +68,45 @@ def rgb2hex(arr):
             blue = arr[i,j,2]
             ret[i,j] = red + green + blue
                        
-                       
     return ret
+
+
+def draw_circle(buf, center, r, color):
+    """
+    Description:
+        Draws a circle at a given center of radius r on a visual buffer buf
+        colored color
+    """
+    center_x = center[0]
+    center_y = center[1]
+    x = r
+    y = 0
+
+    buf[x+center_x][y+center_y] = color
+    if r > 0:
+        buf[x+center_x][-y+center_y] = color
+        buf[y+center_x][x+center_y] = color
+        buf[-y+center_x][x+center_y] = color
+
+    P = 1 - r
+    while x > y:
+        y += 1
+        if P <= 0:
+            P = P + 2*y + 1
+        else:
+            x -= 1
+            P = P + 2*y - 2*x + 1
+
+        if x < y:
+            break
+
+        buf[x+center_x][y+center_y] = color
+        buf[-x+center_x][y+center_y] = color
+        buf[x+center_x][-y+center_y] = color
+        buf[-x+center_x][-y+center_y] = color
+        
+        if x != y:
+            buf[y+center_x][x+center_y] = color
+            buf[-y+center_x][x+center_y] = color
+            buf[y+center_x][-x+center_y] = color
+            buf[-y+center_x][-x+center_y] = color
