79cc82f7e16b7991a67e5be3f5dbd1640b663093
The-krolik <100061578+The-krolik@users.noreply.github.com>
2022-04-14 16:46:22 -0500

    Update tests.py
    updated function calls for black changes

--- tests.py
+++ tests.py
@@ -6,21 +6,21 @@ from typing import Type
 def test_audio():
     a = dpp.Audio()
 
-    assert a.getBitNumber() == 16
-    assert a.getSampleRate() == 44100
+    assert a.get_bit_number() == 16
+    assert a.get_sample_rate() == 44100
     assert a.waveform == a.waves.sin
 
-    a.setWaveForm(a.waves.square)
+    a.set_waveform(a.waves.square)
     assert a.waveform == a.waves.square
 
     with pytest.raises(Exception) as e_info:
-        a.setWaveForm(a.waves.cos)
+        a.set_waveform(a.waves.cos)
     assert a.waveform == a.waves.square
 
 
 def test_vbuffer():
     vb = dpp.VBuffer()
-    x, y = vb.getDimensions()
+    x, y = vb.get_dimensions()
     assert x > 0
     assert y > 0
     assert type(x) is int
@@ -48,51 +48,51 @@ def test_vbuffer():
         vb = dpp.VBuffer([200, 200.5])
 
     vb = dpp.VBuffer([1920, 1080])
-    x, y = vb.getDimensions()
+    x, y = vb.get_dimensions()
     assert type(x) is int
     assert type(y) is int
     assert x == 1920
     assert y == 1080
 
     vb = dpp.VBuffer()
-    x, y = vb.getDimensions()
+    x, y = vb.get_dimensions()
     assert type(x) is int
     assert type(y) is int
     assert x == 800
     assert y == 600
 
     vb = dpp.VBuffer([800, 600])
-    assert vb.getPixel([400, 300]) == 0
+    assert vb.get_pixel([400, 300]) == 0
 
     with pytest.raises(ValueError) as e_info:
-        vb.writePixel([-300, 9], 16777215)
+        vb.write_pixel([-300, 9], 16777215)
 
     with pytest.raises(ValueError) as e_info:
-        vb.writePixel([300, -200], 16777215)
+        vb.write_pixel([300, -200], 16777215)
 
     with pytest.raises(TypeError) as e_info:
-        vb.writePixel([300.1, 200], 16777215)
+        vb.write_pixel([300.1, 200], 16777215)
 
     with pytest.raises(TypeError) as e_info:
-        vb.writePixel([20, 799.5], 16777215)
+        vb.write_pixel([20, 799.5], 16777215)
 
     with pytest.raises(ValueError) as e_info:
-        vb.writePixel([399, 299], 16777216)
-    assert vb.getPixel([399, 299]) == 0
+        vb.write_pixel([399, 299], 16777216)
+    assert vb.get_pixel([399, 299]) == 0
 
     with pytest.raises(ValueError) as e_info:
-        vb.writePixel([399, 299], -1)
-    assert vb.getPixel([399, 299]) == 0
+        vb.write_pixel([399, 299], -1)
+    assert vb.get_pixel([399, 299]) == 0
 
     with pytest.raises(TypeError) as e_info:
-        vb.writePixel([399, 299], 100.5)
-    assert vb.getPixel([399, 299]) == 0
+        vb.write_pixel([399, 299], 100.5)
+    assert vb.get_pixel([399, 299]) == 0
 
-    vb.writePixel([0, 0], 16777215)
-    assert vb.getPixel([0, 0]) == 16777215
+    vb.write_pixel([0, 0], 16777215)
+    assert vb.get_pixel([0, 0]) == 16777215
 
-    vb.writePixel([799, 599], 0xFFFFFF)
-    assert vb.getPixel([0, 0]) == 16777215
+    vb.write_pixel([799, 599], 0xFFFFFF)
+    assert vb.get_pixel([0, 0]) == 16777215
 
-    vb.clearBuffer()
+    vb.clear()
     assert vb.buffer.sum() == 0
