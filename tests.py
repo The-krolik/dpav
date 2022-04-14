ced9cd32c50b74597ede1cc760edc23c4a2837ee
The-krolik <100061578+The-krolik@users.noreply.github.com>
2022-04-14 15:11:51 -0500

    Update tests.py
    deleted extraneous white space for black compliance

--- tests.py
+++ tests.py
@@ -2,35 +2,21 @@ import pytest
 import directpythonplatform as dpp
 from typing import Type
 
+
 def test_audio():
     a = dpp.Audio()
 
     assert a.getBitNumber() == 16
-    assert a.getSampleRate() == 44100
-
-    #a.setAudioDevice(1)
-    #assert a.getAudioDevice() == 1
-
-    #with pytest.raises(ValueError) as e_info:
-    #    a.setAudioDevice(-1)
-    #assert a.getAudioDevice() == 1
-    
-    #with pytest.raises(TypeError) as e_info:
-    #    a.setAudioDevice(1.5)
-    #assert a.getAudioDevice() == 1
-    
+    assert a.getSampleRate() == 44100   
     assert a.waveform == a.waves.sin
-    
+
     a.setWaveForm(a.waves.square)
     assert a.waveform == a.waves.square
-    
+
     with pytest.raises(Exception) as e_info:
-        a.setWaveForm(a.waves.cos)   
+        a.setWaveForm(a.waves.cos)
     assert a.waveform == a.waves.square
 
-
-    # playSound tests
-    
 
 def test_vbuffer():
     vb = dpp.VBuffer()
@@ -42,32 +28,32 @@ def test_vbuffer():
 
     with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([0, 0])
-        
+
     with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([0, 600])
-        
+
     with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([800, 0])
-        
+
     with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([9999, 9999])
-    
+
     with pytest.raises(ValueError) as e_info:
         vb = dpp.VBuffer([-100, -100])
-        
+
     with pytest.raises(TypeError) as e_info:
         vb = dpp.VBuffer([100.9, 200])
-        
+
     with pytest.raises(TypeError) as e_info:
         vb = dpp.VBuffer([200, 200.5])
-     
+
     vb = dpp.VBuffer([1920, 1080])
     x, y = vb.getDimensions()
     assert type(x) is int
     assert type(y) is int
     assert x == 1920
     assert y == 1080
-    
+
     vb = dpp.VBuffer()
     x, y = vb.getDimensions()
     assert type(x) is int
@@ -77,23 +63,23 @@ def test_vbuffer():
 
     vb = dpp.VBuffer([800, 600])
     assert vb.getPixel([400, 300]) == 0
-    
+
     with pytest.raises(ValueError) as e_info:
         vb.writePixel([-300, 9], 16777215)
-    
+
     with pytest.raises(ValueError) as e_info:
         vb.writePixel([300, -200], 16777215)
-        
+
     with pytest.raises(TypeError) as e_info:
         vb.writePixel([300.1, 200], 16777215)
-        
+
     with pytest.raises(TypeError) as e_info:
         vb.writePixel([20, 799.5], 16777215)
-    
+
     with pytest.raises(ValueError) as e_info:
         vb.writePixel([399, 299], 16777216)
     assert vb.getPixel([399, 299]) == 0
-    
+
     with pytest.raises(ValueError) as e_info:
         vb.writePixel([399, 299], -1)
     assert vb.getPixel([399, 299]) == 0
@@ -102,11 +88,11 @@ def test_vbuffer():
         vb.writePixel([399, 299], 100.5)
     assert vb.getPixel([399, 299]) == 0
 
-    vb.writePixel([0,0], 16777215)
+    vb.writePixel([0, 0], 16777215)
     assert vb.getPixel([0, 0]) == 16777215
-    
+
     vb.writePixel([799, 599], 0xffffff)
     assert vb.getPixel([0, 0]) == 16777215
-    
+
     vb.clearBuffer()
     assert vb.buffer.sum() == 0
