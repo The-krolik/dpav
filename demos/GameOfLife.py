07b2391490b893fa64175b4ba06928c9cbd5270e
Cody <chavilan@icloud.com>
2022-04-15 17:50:40 -0500

    updated gol

--- demos/GameOfLife.py
+++ demos/GameOfLife.py
@@ -3,23 +3,25 @@ import time
 
 
 class GameOfLife:
-    def __init__(dimensions, scale, alive=0xFFFFFF, dead=0x000000):
+    def __init__(self, dimensions, scale, alive=0xFFFFFF, dead=0x000000):
         self.alive = alive
         self.dead = dead
         self.scale = scale
         self.cells = dpp.VBuffer(dimensions)
         self.cells.fill(dead)
         self.next_gen = dpp.VBuffer(dimensions)
-        self.dpp.Window(cells, scale)
+        self.window = dpp.Window(self.cells, scale)
 
 
     def start(self, points):
         for (x, y) in points:
-            cells[x][y] = self.alive
+            self.cells[x][y] = self.alive
         self.window.open()
 
 
     def _get_cell_bin(self, x, y):
+        if (x >= len(self.cells)) or (y >= len(self.cells[x])):
+            return 0
         if self.cells[x][y] == self.alive:
             return 1
         else:
@@ -39,15 +41,15 @@ class GameOfLife:
 
 
     def step(self):
-        self.next_gen.fill(dead)
-        for i in range(len(grid)):
-            for j in range(len(vb[i])):
+        self.next_gen.fill(self.dead)
+        for i in range(len(self.cells)):
+            for j in range(len(self.cells[i])):
                 sum = self._sum_neighbors(i, j)
-                if (cells[i][j] == alive) and ((sum == 2) or (sum == 3)):
-                    next_gen[i][j] = alive
-                elif (cells[i][j] == dead) and (sum == 3):
-                    next_gen[i][j] = alive
-        self.cells = self.next_gen
+                if (self.cells[i][j] == self.alive) and ((sum == 2) or (sum == 3)):
+                    self.next_gen[i][j] = self.alive
+                elif (self.cells[i][j] == self.dead) and (sum == 3):
+                    self.next_gen[i][j] = self.alive
+        self.cells, self.next_gen = self.next_gen, self.cells
         self.window.write_to_screen()
 
 
@@ -55,15 +57,14 @@ class GameOfLife:
         self.window.close()
 
 
-if __name__ == "__main__":
-    main()
-
-
 def main():
     dimensions = (100, 100)
-    game = GameOfLife(dimensions, 10)
+    game = GameOfLife(dimensions, 7)
     initial_points = [(49,50), (50,50), (51,50)]
     game.start(initial_points)
-    while True:
-        time.sleep(5)
+    while game.window._isopen:
+        time.sleep(1)
         game.step()
+
+if __name__ == "__main__":
+    main()
