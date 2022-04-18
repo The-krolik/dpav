7f7b8707d5e6b95ac0323d2cde30f145f6aa9dd2
chandlerstewart <user email>
2022-04-18 15:31:50 -0500

    piano.py ran through black

--- demos/piano.py
+++ demos/piano.py
@@ -8,6 +8,7 @@ from vbuffer import VBuffer
 from audio import Audio
 import utility as util
 
+
 class Key:
     def __init__(self, loc, keytype, note, octave):
         self.loc = loc
@@ -18,79 +19,121 @@ class Key:
 
 def playKey(pos, wsize, bKeys, wKeys):
 
-    x,y = pos[0],pos[1]
-    bInd = min(math.floor(x/wsize * 5/7), len(bKeys)-1)
-    wInd = math.floor(x/wsize)
+    x, y = pos[0], pos[1]
+    bInd = min(math.floor(x / wsize * 5 / 7), len(bKeys) - 1)
+    wInd = math.floor(x / wsize)
     bKey = bKeys[bInd]
     wKey = wKeys[wInd]
 
     key = wKey
-    if x >= bKey.loc[0][0] and x <= bKey.loc[1][0] and y >= bKey.loc[0][1] and y <= bKey.loc[1][1]:
+    if (
+        x >= bKey.loc[0][0]
+        and x <= bKey.loc[1][0]
+        and y >= bKey.loc[0][1]
+        and y <= bKey.loc[1][1]
+    ):
         key = bKey
-        
+
     frequency = math.ceil(util.get_note_from_string(key.note, key.octave))
     mySound = Audio()
-    mySound.play_sound(frequency, .25)
+    mySound.play_sound(frequency, 0.25)
 
 
 def drawWhiteKeys(buf, numKeys, keySize, keySpacing):
-   
+
     x = 0
     wKeys = []
     dim = buf.get_dimensions()
-    
+
     drawkey = True
-    for i in range(numKeys*2 - 1):
+    for i in range(numKeys * 2 - 1):
         if drawkey:
-            start = (x,0)
-            end = (x+keySize,dim[1])
-            util.draw_rectangle(buf,0xffffff,start,end)
-            wKeys.append(Key([start,end],"white",None,None))
+            start = (x, 0)
+            end = (x + keySize, dim[1])
+            util.draw_rectangle(buf, 0xFFFFFF, start, end)
+            wKeys.append(Key([start, end], "white", None, None))
             x += keySize
         else:
-            start = (x,0)
-            end = (x+keySpacing,dim[1])
-            util.draw_rectangle(buf,0x000000,start,end)
+            start = (x, 0)
+            end = (x + keySpacing, dim[1])
+            util.draw_rectangle(buf, 0x000000, start, end)
             x += keySpacing
 
         drawkey = True if drawkey == False else False
 
     return wKeys
 
+
 def drawBlackKeys(buf, numWKeys, wkeySize, wkeySpacing):
 
     drawkey = True
-    bkeySize = round(wkeySize * 2 /3)
-    if (bkeySize % 2==0): bkeySize += 1
+    bkeySize = round(wkeySize * 2 / 3)
+    if bkeySize % 2 == 0:
+        bkeySize += 1
 
     dim = buf.get_dimensions()
     bKeys = []
-    x = wkeySize - round(bkeySize/2)
-    for i in range(1,round(numWKeys*5/7)):
-        start = (x,0)
-        end = (x+bkeySize,round(dim[1] * 2/3))
-        util.draw_rectangle(buf,0x000000,start,end)
-        bKeys.append(Key([start,end],"black",None,None))
-
+    x = wkeySize - round(bkeySize / 2)
+    for i in range(1, round(numWKeys * 5 / 7)):
+        start = (x, 0)
+        end = (x + bkeySize, round(dim[1] * 2 / 3))
+        util.draw_rectangle(buf, 0x000000, start, end)
+        bKeys.append(Key([start, end], "black", None, None))
 
         x += wkeySize + wkeySpacing
-        if (i % 5 in [0,2]):
+        if i % 5 in [0, 2]:
             x += wkeySize + wkeySpacing
 
     return bKeys
 
 
 def getWhiteNotes():
-    return ['C', 'D', 'E', 'F', 'G', 'A', 'B',
-            'C', 'D', 'E', 'F', 'G', 'A', 'B',
-            'C', 'D', 'E', 'F', 'G', 'A', 'B', 'C']
+    return [
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "A",
+        "B",
+        "C",
+    ]
+
 
 def getBlackNotes():
-    return ['C#', 'D#', 'F#', 'G#', 'A#',
-            'C#', 'D#', 'F#', 'G#', 'A#',
-            'C#', 'D#', 'F#', 'G#', 'A#']
-    
-    
+    return [
+        "C#",
+        "D#",
+        "F#",
+        "G#",
+        "A#",
+        "C#",
+        "D#",
+        "F#",
+        "G#",
+        "A#",
+        "C#",
+        "D#",
+        "F#",
+        "G#",
+        "A#",
+    ]
+
+
 if __name__ == "__main__":
 
     white_keys = 22
@@ -98,48 +141,82 @@ if __name__ == "__main__":
     wkey_spacing = 3
 
     windowx = (wkey_size * white_keys) + ((white_keys - 1) * wkey_spacing)
-    buf = VBuffer((windowx,300))
+    buf = VBuffer((windowx, 300))
 
-    wKeys = drawWhiteKeys(buf,white_keys,wkey_size,wkey_spacing)
-    bKeys = drawBlackKeys(buf,white_keys,wkey_size,wkey_spacing)
+    wKeys = drawWhiteKeys(buf, white_keys, wkey_size, wkey_spacing)
+    bKeys = drawBlackKeys(buf, white_keys, wkey_size, wkey_spacing)
 
     wNotes = getWhiteNotes()
     bNotes = getBlackNotes()
 
-
     for i in range(len(wNotes)):
-         wKeys[i].note = wNotes[i]
+        wKeys[i].note = wNotes[i]
     for i in range(len(bNotes)):
-         bKeys[i].note = bNotes[i]
+        bKeys[i].note = bNotes[i]
     for i in range(len(wNotes)):
-         wKeys[i].octave = math.ceil((i+3)/7+2)
+        wKeys[i].octave = math.ceil((i + 3) / 7 + 2)
     for i in range(len(bNotes)):
-         bKeys[i].octave = math.ceil((i+2)/5+2)
+        bKeys[i].octave = math.ceil((i + 2) / 5 + 2)
 
-
-    wevents = ['1','2','3','4','5','6','7','8','9','0','-','=',
-                    'q','w','e','r','t','y','u','i','o','p']
-    bevents = ['a','s','d','f','g','h','j','k','l','z',
-               'x','c','v','b','n']
-
+    wevents = [
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "0",
+        "-",
+        "=",
+        "q",
+        "w",
+        "e",
+        "r",
+        "t",
+        "y",
+        "u",
+        "i",
+        "o",
+        "p",
+    ]
+    bevents = [
+        "a",
+        "s",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "l",
+        "z",
+        "x",
+        "c",
+        "v",
+        "b",
+        "n",
+    ]
 
     window = Window(buf)
     window.open()
     mySound = Audio()
 
     while window.is_open():
-        for i,event in enumerate(wevents):
+        for i, event in enumerate(wevents):
             if event in window.eventq:
                 key = wKeys[i]
                 frequency = math.ceil(util.get_note_from_string(key.note, key.octave))
-                mySound.play_sound(frequency, .25)
+                mySound.play_sound(frequency, 0.25)
 
-        for i,event in enumerate(bevents):
+        for i, event in enumerate(bevents):
             if event in window.eventq:
                 key = wKeys[i]
                 frequency = math.ceil(util.get_note_from_string(key.note, key.octave))
-                mySound.play_sound(frequency, .25)
+                mySound.play_sound(frequency, 0.25)
 
-        if 'mouse' in window.eventq:
+        if "mouse" in window.eventq:
             pos = window.get_mouse_pos()
-            playKey(pos,wkey_size+wkey_spacing,bKeys,wKeys)
+            playKey(pos, wkey_size + wkey_spacing, bKeys, wKeys)
