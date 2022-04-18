2be2b05cc4551928ace651651e50a18f21ebea0c
George Gannon <sd.ggannon@mtsu.edu>
2022-04-17 23:39:38 -0500

    Continous graph, old method added as option
    Pretty happy with this tbh. Very simple in terms of how it works

--- graphcalc.py
+++ graphcalc.py
@@ -14,10 +14,11 @@ class dppgraph:
         self.dimensions=dimensions
 
         self.linecolor=0xFF0000
-        self.gridcolor=0x000000
+        self.gridcolor=0x111111
         self.bgcolor=0xFFFFFF
         self.window=dp.Window(self.bg)
         self.windowopen=False
+        self.cont=True
 
     def draw(self, graphee, bounds):
         self.bg.fill(self.bgcolor)
@@ -32,21 +33,32 @@ class dppgraph:
     
     def drawFunc(self, vb, graphed):
         xpts=np.linspace(self.bounds[0][0], self.bounds[0][1], self.dimensions[1])
+        graphed.xtble=xpts
         evaluated=np.empty(xpts.size)
-
         for i,eachpt in enumerate(xpts):
             x=eachpt
             evaluated[i]=eval(graphed.func)
 
+        graphed.ytble=evaluated
+        
         vlinecount=self.bounds[0][1]-self.bounds[0][0]
         pixels_per_x=self.dimensions[0]//vlinecount
         
         unitrate=1/pixels_per_x # smallest unit one pixel on the y can represent
         #to get pixel representation: divide Y-lowerbound by unitrate
         for i,each in enumerate(evaluated):
+            if(i==0):continue
+
             calc_pixel=(each-self.bounds[1][0])//unitrate
+            calc_pixel_bef=(evaluated[i-1]-self.bounds[1][0])//unitrate
             if(calc_pixel<=self.dimensions[1] and calc_pixel>0):
-                vb.write_pixel([i,self.dimensions[1]-int(round(calc_pixel))], self.linecolor) 
+                if self.cont:
+                    try:
+                        utility.draw_line(vb, [i, self.dimensions[1]-int(round(calc_pixel)) ],[i-1, self.dimensions[1]-int(round(calc_pixel_bef))], self.linecolor)
+                    except:
+                        pass
+                else:
+                    vb.write_pixel([i,self.dimensions[1]-int(round(calc_pixel))],self.linecolor)
 
             
     def drawGrid(self, vb):
@@ -74,7 +86,8 @@ class dppgraph:
 class dppgraphee:
     def __init__(self, function):
         self.func = parser.expr(function).compile()
-        print(function)
+        self.xtble=None
+        self.ytble=None
         pass
 
 
@@ -82,12 +95,15 @@ class dppgraphee:
 
 
 def main():
-    parser=argparse.ArgumentParser(description='Direct Python Platform graphing utility')
-    parser.add_argument('--function=', dest= 'func', metavar='func', type=str, help='Python parseable expression in terms of x. Supports sin, cos, and tan')
-    parser.add_argument('--bounds=', dest='bounds',metavar='x0 x1 y0 y1', type=str,help='Mathematical bounds for the graph')
-    parser.add_argument('--graph=', dest='go', metavar= 'graph', action='store_const',const=True, help='Graph the given function within --function')
-    parser.add_argument('--dimensions=', dest='dims', metavar='l by w', type=str, help='Dimensions for specifiying the window in terms of pixels.')
+    parser=argparse.ArgumentParser(description='Direct Python Platform graphing utility. Graphs a given function specified from command line arguments')
+    parser.add_argument('--function=', dest= 'func', metavar='func', type=str, default="x**2", help='Python parseable expression in terms of x. Supports sin, cos, and tan')
+    parser.add_argument('--bounds=', dest='bounds',metavar='x0 x1 y0 y1', default="-10 10 -10 10",type=str,help='Mathematical bounds for the graph')
+    parser.add_argument('--graph=', dest='go', metavar= 'graph', action='store_const', const=True, help='Graph the given function within --function')
+    parser.add_argument('--dimensions=', dest='dims', metavar='l by w', type=str, default='800 800', help='Dimensions for specifiying the window in terms of pixels.')
     parser.add_argument('--darkmode=', dest='darkmode', metavar='Darkmode', action='store_const', const=True, help='In dark mode, graphed function is green, background is black, and grid is white')
+    parser.add_argument('--outputtable', dest='table', action='store_const', const=True, help='Outputs a table of values in stdout: x0...x1')
+    parser.add_argument('--points', dest='points', action='store_const', const=True, help='For functions with rapid changes in direction, it may help to show graph as points instead of continuous lines.')
+
     args=parser.parse_args()
     if(args.go):
         graph(args)
@@ -109,10 +125,19 @@ def graph(args):
         calc.linecolor=0x00FF00
         calc.bgcolor=0x000000
         calc.gridcolor=0x4F4F4F
+    
+    if(args.points):
+        calc.cont=False
 
     calc.draw(graphed, calc.bounds)
     while calc.window.is_open():
         if 'q' in calc.window.eventq:
+            if(args.table):
+                print("X        Y")
+                print("----------")
+                string="{x:.4f} {y:.4f}"
+                for i,each in enumerate(graphed.xtble):
+                    print(string.format(x=each, y=graphed.ytble[i]))
             calc.window.close()
 if __name__=='__main__':
     main()
