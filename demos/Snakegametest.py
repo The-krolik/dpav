8b1e1856bba20774b43bdaade6c7069b43f8170f
Angelo C. Tammaro <chaztammaro@yahoo.com>
2022-04-19 21:39:10 -0500

    Moved snakegame back to demos

--- demos/Snakegametest.py
+++ demos/Snakegametest.py
@@ -1,124 +1,138 @@
-import pygame
-import time
+from directpythonplatform import *
+from utility import draw_rectangle
 import random
-import directpythonplatform as dpp
- 
-pygame.init()
- 
-#converted the colors to HEX values, as needed for vbuffer
-#white = (255, 255, 255)
-white = 0xFFFFFF
-#yellow = (255, 255, 102)
-yellow = 0xFFFF00
-#black = (0, 0, 0)
-black = 0x000000
-#red = (213, 50, 80)
-red = 0xFF0000
-#green = (0, 255, 0)
-green = 0x008000
-#blue = (50, 153, 213)
-blue = 0x0000FF
- 
-#dis_width = 600
-#dis_height = 400
-dimensions = [600, 400]
- 
-#dis = pygame.display.set_mode((dis_width, dis_height))
-vb = dpp.VBuffer(dimensions)
-dis = dpp.Window(vb, 1)
- 
 clock = pygame.time.Clock()
- 
+
+
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pt1 = (int(x[0]),int(x[1]))
+        pt2 = (int(x[0] + snake_block),int(x[1] + snake_block))
+        draw_rectangle(window.vbuffer,white,pt1,pt2)
+        
+
+white = 0xffffff
+black = 0x000000
+red = 0xFF0000
+    
+x_dim = 600
+y_dim = 400
+
+window = Window(VBuffer((x_dim,y_dim)))
+game_over = False
+game_close = False
+
+x1 = x_dim / 2
+y1 = y_dim / 2
+
+count = 1
+x1_change = 0
+y1_change = 0
+
 snake_block = 10
 snake_speed = 15
 
- 
-def our_snake(snake_block, snake_list):
-    for x in snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
-        #draw_rectangle(vb, black, x[0], x[1])    #our function, have yet to test it so I commented it out
- 
-#unchanged
-def gameLoop():
-    game_over = False
-    game_close = False
- 
-    x1 = dimensions[0] / 2
-    y1 = dimensions[1] / 2
- 
-    x1_change = 0
-    y1_change = 0
- 
-    snake_List = []
-    Length_of_snake = 1
- 
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
- 
-    while not game_over:
- 
-        while game_close == True:
-            dis.fill(blue)
-            Your_score(Length_of_snake - 1)
-            pygame.display.update()
- 
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        game_over = True
-                        game_close = False
-                    if event.key == pygame.K_c:
-                        gameLoop()
- 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game_over = True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    x1_change = -snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
-                    y1_change = 0
-                elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
-                    x1_change = 0
-                elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
-                    x1_change = 0
- 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
-            game_close = True
+snake_List = []
+Length_of_snake = 1
+
+window.open()
+gotFood = False
+start = True
+
+window.vbuffer[:] = black
+foodx = round(random.randrange(0, x_dim - snake_block) / 10.0) * 10.0
+foody = round(random.randrange(0, y_dim - snake_block) / 10.0) * 10.0
+pt1 = (int(foodx),int(foody))
+pt2 = (int(foodx + snake_block),int(foody + snake_block))
+draw_rectangle(window.vbuffer,red,pt1,pt2)
+
+while window.is_open():
+    
+    if 'q' in window.eventq:
+        game_over = True
+        game_close = False
+        quit()
+    elif 'w' in window.eventq: #up
+        x1_change = 0
+        y1_change = -snake_block
+    elif 'a' in window.eventq: #left
+        x1_change = -snake_block
+        y1_change = 0
+    elif 's' in window.eventq: #down
+        x1_change = 0
+        y1_change = snake_block
+    elif 'd' in window.eventq: #right
+        x1_change = snake_block
+        y1_change = 0
+    
+    x1 += x1_change
+    y1 += y1_change
+    window.vbuffer[:] = black
+
+
+    if x1 > x_dim or x1 < 0 or y1 > y_dim or y1 < 0:
+        #resets the snake
+        window.vbuffer[:] = black
+        x1_change = 0
+        y1_change = 0
+        x1 = x_dim / 2
+        y1 = y_dim / 2
         x1 += x1_change
         y1 += y1_change
-        dis.fill(blue)
-        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
-        #draw_rectangle(vb, black, x[0], x[1])   #same as above
-        snake_Head = []
-        snake_Head.append(x1)
-        snake_Head.append(y1)
-        snake_List.append(snake_Head)
-        if len(snake_List) > Length_of_snake:
-            del snake_List[0]
- 
-        for x in snake_List[:-1]:
-            if x == snake_Head:
-                game_close = True
- 
-        our_snake(snake_block, snake_List)
-        Your_score(Length_of_snake - 1)
- 
-        pygame.display.update()
- 
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-            Length_of_snake += 1
- 
-        clock.tick(snake_speed)
- 
-    pygame.quit()
-    quit()
- 
- 
-gameLoop()
+        snake_Head = [x1, y1]
+        Length_of_snake = 1
+        snake_List = []
+        our_snake(snake_block,snake_List)
+        #resets the food
+        foodx = round(random.randrange(0, x_dim - snake_block) / 10.0) * 10.0
+        foody = round(random.randrange(0, y_dim - snake_block) / 10.0) * 10.0
+        pt1 = (int(foodx),int(foody))
+        pt2 = (int(foodx + snake_block),int(foody + snake_block))
+        draw_rectangle(window.vbuffer,red,pt1,pt2)
+    
+    
+    snake_Head = [x1, y1]
+    snake_List.append(snake_Head)
+    
+    snake_Head = snake_List[0]
+    draw_rectangle(window.vbuffer,red,(int(foodx),int(foody)),(int(foodx + snake_block), int(foody + snake_block)))
+    
+    
+    if len(snake_List) > Length_of_snake:
+        del snake_List[0]
+    
+    if len(snake_List) > 1:
+        if snake_Head in snake_List[1:]:
+            #resets the snake
+            window.vbuffer[:] = black
+            x1_change = 0
+            y1_change = 0
+            x1 = x_dim / 2
+            y1 = y_dim / 2
+            x1 += x1_change
+            y1 += y1_change
+            snake_Head = [x1, y1]
+            Length_of_snake = 1
+            snake_List = []
+            our_snake(snake_block,snake_List)
+            #resets the food
+            foodx = round(random.randrange(0, x_dim - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, y_dim - snake_block) / 10.0) * 10.0
+            pt1 = (int(foodx),int(foody))
+            pt2 = (int(foodx + snake_block),int(foody + snake_block))
+            draw_rectangle(window.vbuffer,red,pt1,pt2)
+
+    for x in snake_List[:-1]:
+        if x == snake_Head:
+            game_close = True
+            
+    our_snake(snake_block,snake_List)
+    
+    clock.tick(snake_speed)
+
+    if x1 == foodx and y1 == foody:
+        gotFood = True
+        foodx = round(random.randrange(0, x_dim - snake_block) / 10.0) * 10.0
+        foody = round(random.randrange(0, y_dim - snake_block) / 10.0) * 10.0
+        Length_of_snake += 1
