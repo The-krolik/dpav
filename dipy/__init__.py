521b9d2d52357fb71ec2a33fb56267d81ff60e39
Cody <chavilan@icloud.com>
2022-04-28 20:32:17 -0500

    psudo fixed dipy/__init__.py

--- dipy/__init__.py
+++ dipy/__init__.py
@@ -1,1 +1,7 @@
-__all__=["vbuffer", "window", "audio", "utility"]
+from .vbuffer import VBuffer
+from .window import Window
+from .audio import Audio
+from .utility import *
+
+
+__all__=["vbuffer.VBuffer", "window", "audio", "utility"]
