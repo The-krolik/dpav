219101dc24e76ef2088cb157c69d6c40a8db5c63
Cody <chavilan@icloud.com>
2022-04-29 01:33:28 -0500

    tests should *actually* work now

--- dpav/__init__.py
+++ dpav/__init__.py
@@ -3,4 +3,4 @@ from .window import Window
 from .audio import Audio
 from .utility import *
 
-__all__=["VBuffer", "Window", "Audio", "utility"]
+__all__ = ["VBuffer", "Window", "Audio", "utility"]
--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath("../dpav"))
 import pytest
 import dpav as dp
 from typing import Type
