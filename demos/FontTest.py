ba1c113b63c904c90e83391c8292c9664903cb83
bschuderMTSU <bryan.schuder@mtsu.edu>
2022-04-29 11:12:05 -0500

    Clean Phase
    Reworked original ported code to be a little more friendly and accepting of unicode strings in the future.
    Debating classing up things for neatness and keep the verbosity down in usage

--- demos/FontTest.py
+++ demos/FontTest.py
@@ -26,24 +26,24 @@ def main():
         # Draw some text
 
         # Draw individual CGA characters
-        dpp.font_draw_8x8_character(buffer, ord("C"), 152, 120, 0xDDDDDD, 0x112255, dpp.CHARACTER_ROM_CGA_8x8)
-        dpp.font_draw_8x8_character(buffer, ord("G"), 160, 120, 0xDDDDDD, 0x225511, dpp.CHARACTER_ROM_CGA_8x8)
-        dpp.font_draw_8x8_character(buffer, ord("A"), 168, 120, 0xDDDDDD, 0x551122, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_string(buffer, "C", 152, 120, 0xDDDDDD, 0x112255, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_string(buffer, "G", 160, 120, 0xDDDDDD, 0x225511, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_string(buffer, "A", 168, 120, 0xDDDDDD, 0x551122, dpp.CHARACTER_ROM_CGA_8x8)
 
         # Draw individual VGA characters
-        dpp.font_draw_8x16_character(buffer, ord("V"), 152, 150, 0xDDDDDD, 0x113366, dpp.CHARACTER_ROM_VGA_8x16)
-        dpp.font_draw_8x16_character(buffer, ord("G"), 160, 150, 0xDDDDDD, 0x336611, dpp.CHARACTER_ROM_VGA_8x16)
-        dpp.font_draw_8x16_character(buffer, ord("A"), 168, 150, 0xDDDDDD, 0x661133, dpp.CHARACTER_ROM_VGA_8x16)
+        dpp.draw_8x16_string(buffer, "V", 152, 150, 0xDDDDDD, 0x113366, dpp.CHARACTER_ROM_VGA_8x16)
+        dpp.draw_8x16_string(buffer, "G", 160, 150, 0xDDDDDD, 0x336611, dpp.CHARACTER_ROM_VGA_8x16)
+        dpp.draw_8x16_string(buffer, "A", 168, 150, 0xDDDDDD, 0x661133, dpp.CHARACTER_ROM_VGA_8x16)
 
 
         # Draw corner partial drawing characters for testing
-        dpp.font_draw_8x8_character(buffer, ord("X"), -4, -4, 0xEE0000, 0x222222, dpp.CHARACTER_ROM_CGA_8x8)
-        dpp.font_draw_8x8_character(buffer, ord("X"),  316, -4, 0xEE0000, 0x222222, dpp.CHARACTER_ROM_CGA_8x8)
-        dpp.font_draw_8x8_character(buffer, ord("X"), -4, 236, 0xEE0000, 0x222222, dpp.CHARACTER_ROM_CGA_8x8)
-        dpp.font_draw_8x8_character(buffer, ord("X"), 316, 236, 0xEE0000, 0x222222, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_string(buffer, "X", -4, -4, 0xEE0000, 0x222222, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_string(buffer, "X", 316, -4, 0xEE0000, 0x222222, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_string(buffer, "X", -4, 236, 0xEE0000, 0x222222, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_string(buffer, "X", 316, 236, 0xEE0000, 0x222222, dpp.CHARACTER_ROM_CGA_8x8)
 
         # Static Test String
-        dpp.font_draw_8x8_string(buffer, "Static Test String?!", 90, 100, 0x00EE00, 0x104410, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_string(buffer, "Static Test String?!", 90, 100, 0x00EE00, 0x104410, dpp.CHARACTER_ROM_CGA_8x8)
 
         # Scrolling Test String
 
@@ -60,11 +60,11 @@ def main():
             CGAspillOverString = True
 
         # Draw the primary string
-        dpp.font_draw_8x8_string(buffer, CGAstringToScroll, 0 + CGAscrollTextCurrentX, 10, 0x008844, 0x002200, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_string(buffer, CGAstringToScroll, 0 + CGAscrollTextCurrentX, 10, 0x008844, 0x002200, dpp.CHARACTER_ROM_CGA_8x8)
 
         # Draw the spill over string
         if (CGAspillOverString):
-            dpp.font_draw_8x8_string(buffer, CGAstringToScroll, 0 + CGAscrollTextCurrentX - buffer.get_dimensions()[0], 10, 0x008844, 0x002200, dpp.CHARACTER_ROM_CGA_8x8)
+            dpp.draw_8x8_string(buffer, CGAstringToScroll, 0 + CGAscrollTextCurrentX - buffer.get_dimensions()[0], 10, 0x008844, 0x002200, dpp.CHARACTER_ROM_CGA_8x8)
 
         CGAscrollTextCurrentX += 1
 
@@ -81,11 +81,11 @@ def main():
             VGAspillOverString = True
 
         # Draw the primary string
-        dpp.font_draw_8x16_string(buffer, VGAstringToScroll, 0 + VGAscrollTextCurrentX, 200, 0x008844, 0x002200, dpp.CHARACTER_ROM_VGA_8x16)
+        dpp.draw_8x16_string(buffer, VGAstringToScroll, 0 + VGAscrollTextCurrentX, 200, 0x008844, 0x002200, dpp.CHARACTER_ROM_VGA_8x16)
 
         # Draw the spill over string
         if (VGAspillOverString):
-            dpp.font_draw_8x16_string(buffer, VGAstringToScroll, 0 + VGAscrollTextCurrentX - buffer.get_dimensions()[0], 200, 0x008844, 0x002200, dpp.CHARACTER_ROM_VGA_8x16)
+            dpp.draw_8x16_string(buffer, VGAstringToScroll, 0 + VGAscrollTextCurrentX - buffer.get_dimensions()[0], 200, 0x008844, 0x002200, dpp.CHARACTER_ROM_VGA_8x16)
 
         VGAscrollTextCurrentX += 1
 
@@ -94,26 +94,26 @@ def main():
         #  ASCII Box Draw
         #
         # Draw extended ASCII character ╔
-        dpp.font_draw_8x8_character(buffer, 0xC9, 50, 50, 0x1155EE, 0x102200, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_character(buffer, dpp.CHARACTER_ROM_CGA_8x8[0xC9], 50, 50, 0x1155EE, 0x102200)
 
         # Draw extended ASCII character ╚
-        dpp.font_draw_8x8_character(buffer, 0xC8, 50, 80, 0x1155EE, 0x102200, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_character(buffer, dpp.CHARACTER_ROM_CGA_8x8[0xC8], 50, 80, 0x1155EE, 0x102200)
 
         # Draw extended ASCII character ╗
-        dpp.font_draw_8x8_character(buffer, 0xBB, 200, 50, 0x1155EE, 0x102200, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_character(buffer, dpp.CHARACTER_ROM_CGA_8x8[0xBB], 200, 50, 0x1155EE, 0x102200)
 
         # Draw extended ASCII character ╝
-        dpp.font_draw_8x8_character(buffer, 0xBC, 200, 80, 0x1155EE, 0x102200, dpp.CHARACTER_ROM_CGA_8x8)
+        dpp.draw_8x8_character(buffer, dpp.CHARACTER_ROM_CGA_8x8[0xBC], 200, 80, 0x1155EE, 0x102200)
 
         for x in range(58,200,8):
             # Draw extended ASCII character ═
-            dpp.font_draw_8x8_character(buffer, 0xCD, x, 50, 0x1155EE, 0x102200, dpp.CHARACTER_ROM_CGA_8x8)
-            dpp.font_draw_8x8_character(buffer, 0xCD, x, 80, 0x1155EE, 0x102200, dpp.CHARACTER_ROM_CGA_8x8)
+            dpp.draw_8x8_character(buffer, dpp.CHARACTER_ROM_CGA_8x8[0xCD], x, 50, 0x1155EE, 0x102200)
+            dpp.draw_8x8_character(buffer, dpp.CHARACTER_ROM_CGA_8x8[0xCD], x, 80, 0x1155EE, 0x102200)
 
         for y in range(58,80,8):
             # Draw extended ASCII character ═
-            dpp.font_draw_8x8_character(buffer, 0xBA, 50, y, 0x1155EE, 0x102200, dpp.CHARACTER_ROM_CGA_8x8)
-            dpp.font_draw_8x8_character(buffer, 0xBA, 200, y, 0x1155EE, 0x102200, dpp.CHARACTER_ROM_CGA_8x8)
+            dpp.draw_8x8_character(buffer, dpp.CHARACTER_ROM_CGA_8x8[0xBA], 50, y, 0x1155EE, 0x102200)
+            dpp.draw_8x8_character(buffer, dpp.CHARACTER_ROM_CGA_8x8[0xBA], 200, y, 0x1155EE, 0x102200)
 
 
 
