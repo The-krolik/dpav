05cef1b23d7628ad42c021cc50adff39c7d0c301
The-krolik <100061578+The-krolik@users.noreply.github.com>
2022-05-02 13:11:41 -0500

    more black in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ release = "0.0.1"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
