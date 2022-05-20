e247d10df390cabd63f6f551843ee76d00493d58
krolik <the_krolik@icloud.com>
2022-05-20 00:17:26 -0500

    fixed the video prototype

--- dpav/prototypes/video.py
+++ dpav/prototypes/video.py
@@ -6,30 +6,27 @@ import sdl2.ext
 def run():
     sdl2.ext.init()
 
-    window = sdl2.ext.Window("scratch", size=(640, 480))
-    win_surf = window.get_surface()
-    surface = sdl2.SDL_CreateRGBSurface(
-        0,
+    # window = sdl2.ext.Window("scratch", size=(640, 480))
+    window = sdl2.SDL_CreateWindow(
+        b"video_prototype",
+        sdl2.SDL_WINDOWPOS_CENTERED,
+        sdl2.SDL_WINDOWPOS_CENTERED,
         640,
         480,
-        32,
-        0xFF000000,
-        0x00FF0000,
-        0x0000FF00,
-        0x000000FF
+        sdl2.SDL_WINDOW_SHOWN,
+    )
+    # win_surf = window.get_surface()
+    win_surf = sdl2.SDL_GetWindowSurface(window)
+
+    surface = sdl2.SDL_CreateRGBSurfaceWithFormat(
+        0, 640, 480, 24, sdl2.SDL_PIXELFORMAT_RGB888
     )
 
     pixels = sdl2.ext.pixels2d(surface, False)
-    pixels[:][:] = 0xFF00FF00
-    # This blit doesn't work
-    sdl2.SDL_BlitSurface(
-        surface,
-        None,
-        win_surf,
-        None
-    )
+    pixels[:][:] = 0xFFFF00
+    sdl2.SDL_BlitSurface(surface, None, win_surf, None)
 
-    window.show()
+    # window.show()
     running = True
     while running:
         events = sdl2.ext.get_events()
@@ -37,8 +34,10 @@ def run():
             if event.type == sdl2.SDL_QUIT:
                 running = False
                 break
-        window.refresh()
+        # window.refresh()
+        sdl2.SDL_UpdateWindowSurface(window)
     return 0
+
 
 if __name__ == "__main__":
     sys.exit(run())
